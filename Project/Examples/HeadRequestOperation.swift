b7e2f9345873c092e112e87471b30e70e206ed38
Jared Sinclair <jaredsinclair.rn@gmail.com>
2016-10-18 17:21:19 -0500

    Reorganize files.

---
+++ Project/Examples/HeadRequestOperation.swift
@@ -0,0 +1,10 @@
+//
+//  HeadRequestOperation.swift
+//  Project
+//
+//  Created by Jared Sinclair on 10/18/16.
+//  Copyright © 2016 Nice Boy LLC. All rights reserved.
+//
+
+import Foundation
+import AsyncOperations
