c2e9f9166abb04275c712f5f241d6c7945e24799
Jared Sinclair <jaredsinclair.rn@gmail.com>
2016-10-19 09:14:22 -0500

    Add more unit tests.

---
+++ Project/AsyncOperationsTests/AsyncBlockOperationTests.swift
@@ -0,0 +1,56 @@
+//
+//  AsyncBlockOperationTests.swift
+//  Project
+//
+//  Created by Jared Sinclair on 10/19/16.
+//  Copyright © 2016 Nice Boy LLC. All rights reserved.
+//
+
+import XCTest
+@testable import AsyncOperations
+
+class AsyncBlockOperationTests: XCTestCase {
+
+    func test_itExecutesABlock() {
+        let exp = expectation(description: #function)
+        let op = AsyncBlockOperation { finish in
+            exp.fulfill()
+            finish()
+        }
+        op.start()
+        waitForExpectations(timeout: 5)
+    }
+    
+    func test_itExecutesACompletionBlock() {
+        let exp = expectation(description: #function)
+        let op = AsyncBlockOperation { finish in
+            finish()
+        }
+        op.addCompletionHandler {
+            exp.fulfill()
+        }
+        op.start()
+        waitForExpectations(timeout: 5)
+    }
+    
+    func test_itRemainsExecutingUntilFinished() {
+        let exp = expectation(description: #function)
+        var results: [String] = ["idle"]
+        let asyncOp = AsyncBlockOperation { finish in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                results.append("async")
+                finish()
+            }
+        }
+        let syncOp = BlockOperation {
+            results.append("sync")
+            XCTAssertEqual(["idle", "async", "sync"], results)
+            exp.fulfill()
+        }
+        syncOp.addDependency(asyncOp)
+        let queue = OperationQueue()
+        queue.addOperations([asyncOp, syncOp], waitUntilFinished: false)
+        waitForExpectations(timeout: 5)
+    }
+
+}
---
+++ Project/AsyncOperationsTests/AsyncTaskQueueTests.swift
@@ -0,0 +1,9 @@
+//
+//  AsyncTaskQueueTests.swift
+//  AsyncOperationsTests
+//
+//  Created by Jared Sinclair on 10/19/16.
+//  Copyright © 2016 Nice Boy LLC. All rights reserved.
+//
+
+import Foundation
