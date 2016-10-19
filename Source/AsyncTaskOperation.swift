60126f83df980b5f91d730ec0c865e61e19e54df
Jared Sinclair <jaredsinclair.rn@gmail.com>
2016-10-18 20:31:34 -0500

    Add more documentation.

--- Source/AsyncTaskOperation.swift
+++ Source/AsyncTaskOperation.swift
@@ -53,11 +53,26 @@ public class AsyncTaskOperation<Result>: AsyncOperation {
     
     // MARK: Init
     
+    /// Simple initializer.
+    /// 
+    /// - parameter task: The closure in which the work should be executed. This
+    /// will be invoked from a private dispatch queue. Implementations can use
+    /// whatever asynchronous procedures they wish. The only obligation is to
+    /// invoke the finish function passed to the task closure when the task is
+    /// finished. If you fail to invoke the finish function, the task will 
+    /// remain in its executing state indefinitely.
+    /// 
+    /// - 
     public init(task: @escaping Task, cancellation: @escaping Cancellation) {
         self.task = task
         self.cancellation = cancellation
     }
     
+    /// Advanced initializer that adds the first request to the operation as
+    /// soon as it is initialized. This is the only way to guarantee that the
+    /// first request will be successfully added (see the documention for
+    /// `addRequest(preferredPriority:tokenHandler:resultHandler)` for
+    /// additional information about this issue).
     public init(task: @escaping Task, cancellation: @escaping Cancellation, preferredPriority: Operation.QueuePriority, tokenHandler: (RequestToken) -> Void, resultHandler: @escaping ResultHandler) {
         
         self.task = task
@@ -77,6 +92,47 @@ public class AsyncTaskOperation<Result>: AsyncOperation {
     
     // MARK: Public Methods
     
+    /// Adds an additional request to the task.
+    /// 
+    /// - parameter preferredPriority: The task operation's actual priority will
+    /// resolve to the highest preferred priority among its active requests.
+    /// 
+    /// - parameter tokenHandler: A non-escaping closure via which the request 
+    /// token for this request is passed back to the caller. If the token is 
+    /// nil, the request could not be added (see notes below).
+    /// 
+    /// - parameter resultHandler: A closure which will be called when the 
+    /// task operation finishes and produces a result. The same result will be
+    /// distributed to the result handlers of all active requests. This closure
+    /// will be invoked on the main queue.
+    /// 
+    /// ### Avoiding Race Conditions
+    /// 
+    /// Adding requests to a task operation is a highly race-dependent feature.
+    /// Therefore a request cannot be guaranteed to be added. If the operation
+    /// is cancelled (or about to be cancelled) or finished (or about to be
+    /// finished), it cannot accept additional requests.
+    /// 
+    /// Rather than silently failing to add a request, `addRequest()` will
+    /// communicate to the caller whether or not the request was successfully
+    /// added via the `tokenHandler` closure argument. This closure receives an
+    /// optional RequestToken argument. If this argument is `nil`, the request
+    /// failed to be added. If this argument is not nil, the caller can assume
+    /// that the `resultHandler` will be called at some point in the future
+    /// (unless, of course, the operation is later cancelled, in which case the
+    /// result handlers are not called by design).
+    ///
+    /// It is critical to notice that the token handler is non-escaping. This is
+    /// because otherwise `addRequest()` could not guarantee that the calling
+    /// thread has a chance to capture the token value **before the result 
+    /// handler could possibly be called**. If, for example, the request token 
+    /// was a returned value, and if the calling thread was part of a concurrent
+    /// queue, it would be possible for the result handler to be called (on the
+    /// main queue) at the same time as or before the calling thread can store 
+    /// the token value. It is conceivable that an implementation might need to
+    /// use the request token as part of updating some other internal state in a
+    /// manner that is guaranteed to occur before the result handler fires (for 
+    /// example, showing/hiding an indeterminate activity indicator).
     public func addRequest(preferredPriority: Operation.QueuePriority = .normal, tokenHandler: RequestTokenHandler = {_ in}, resultHandler: @escaping ResultHandler) {
         
         doLocked {
@@ -102,6 +158,9 @@ public class AsyncTaskOperation<Result>: AsyncOperation {
         
     }
     
+    /// Cancels the request for `token`, if it still exists.
+    ///
+    /// - parameter token: The request token received when adding the request.
     public func cancelRequest(with token: RequestToken) {
         
         var shouldCancelOperation = false
@@ -130,6 +189,12 @@ public class AsyncTaskOperation<Result>: AsyncOperation {
         }
     }
     
+    /// Adjusts the preferred priority of the request for `token`.
+    ///
+    /// - parameter token: The request token received when adding the request.
+    ///
+    /// - parameter preferredPriority: The task operation's actual priority will
+    /// resolve to the highest preferred priority among its active requests.
     public func adjustPriorityForRequest(with token: RequestToken, preferredPriority: Operation.QueuePriority) {
         doLocked {
             unsafe_requests[token]?.preferredPriority = preferredPriority
